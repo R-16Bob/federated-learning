aafcde977eab6e5064e20f3a5cd2b32746c191b7
R-16Bob <15343443062@163.com>
2024-05-20 12:02:14 +0800

    add:Chinese comments for Fed.py

--- models/Fed.py
+++ models/Fed.py
@@ -8,9 +8,9 @@ from torch import nn
 
 
 def FedAvg(w):
-    w_avg = copy.deepcopy(w[0])
-    for k in w_avg.keys():
-        for i in range(1, len(w)):
-            w_avg[k] += w[i][k]
-        w_avg[k] = torch.div(w_avg[k], len(w))
+    w_avg = copy.deepcopy(w[0])  # 也就是第一个客户端的模型参数（状态字典）
+    for k in w_avg.keys():  # 遍历状态字典的键
+        for i in range(1, len(w)):  # 遍历剩下的客户端
+            w_avg[k] += w[i][k]  # 模型参数相加
+        w_avg[k] = torch.div(w_avg[k], len(w))  # 模型参数相加后除以客户端个数，做FedAvg
     return w_avg
